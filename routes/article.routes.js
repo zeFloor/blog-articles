0b10016d4889a2408f1ce4504dccbe01727fd3e4
zeFloor <erviss1@gmail.com>
2022-02-23 11:41:05 +0200

    connection to database

--- routes/article.routes.js
+++ routes/article.routes.js
@@ -1,13 +1,30 @@
 const express = require('express')
+const Article = require('../models/article')
 
 const router = express.Router()
 
 router.get('/new', (req, res) => {
-    res.render('new')
+    res.render('new', { article: new Article() })
 })
 
-router.post('/', (req, res) => {
-    
+router.get('/:id', async (req, res) => {
+    const article = await Article.findById(req.body.id)
+    if(article === null) res.redirect('/')
+    res.render('show', { article: article })
+})
+
+router.post('/', async (req, res) => {
+    let article = new Article({
+        title: req.body.title,
+        description: req.body.description,
+        markdown: req.body.markdown,
+    })
+    try{
+       article = await article.save()
+       res.redirect(`/articles/${article.id}`)
+    } catch(e) {
+        res.render('new', { article: article })
+    }
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const articleRouter = require('./routes/article.routes')
+const mongoose = require('mongoose')
 
+mongoose.connect("mongodb+srv://dbErvinas:dbSlaptazodis@cluster0.pyakb.mongodb.net/markdownBlog?retryWrites=true&w=majority")
 const app = express()
 
 app.set('view engine', 'ejs')
-
-app.use('/articles', articleRouter)
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
     let articles = [{
@@ -24,5 +25,7 @@ app.get('/', (req, res) => {
 
     res.render('index', { articles: articles })
 })
+
+app.use('/articles', articleRouter)
 
 app.listen(5000, console.log('Listening on port: 5000'))
