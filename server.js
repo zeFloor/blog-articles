7fee15ad11bc6a75765dbf8f65fc78b91b07e056
zeFloor <erviss1@gmail.com>
2022-02-23 10:41:24 +0200

    index of a blog page

--- server.js
+++ server.js
@@ -1,5 +1,28 @@
 const express = require('express')
+const articleRouter = require('./routes/article.routes')
 
 const app = express()
+
+app.set('view engine', 'ejs')
+
+app.use('/articles', articleRouter)
+
+app.get('/', (req, res) => {
+    let articles = [{
+        'title': 'Article1',
+        'description': 'Article description',
+        'createdAt': new Date(),
+        'markdow': 'This is the markdown',
+    },
+    {
+        'title': 'Article2',
+        'description': 'Article2 description',
+        'createdAt': new Date(),
+        'markdow': 'This is the markdown',
+    },
+]
+
+    res.render('index', { articles: articles })
+})
 
 app.listen(5000, console.log('Listening on port: 5000'))
