deca53a3e393973582137a1d65c65f636bb126b2
zeFloor <erviss1@gmail.com>
2022-02-23 11:10:11 +0200

    new article form

--- routes/article.routes.js
+++ routes/article.routes.js
@@ -2,8 +2,12 @@ const express = require('express')
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.send('Articles')
+router.get('/new', (req, res) => {
+    res.render('new')
+})
+
+router.post('/', (req, res) => {
+    
 })
 
 module.exports = router
